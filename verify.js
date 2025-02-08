5efec77d2e5a519026888b0583f60002a16c8164
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-08 22:13:08 +0100

    Setting export feature in module file

--- verify.js
+++ verify.js
@@ -1,7 +1,9 @@
-function checkEmail(correo) {
+export function checkEmail(correo) {
     const Regex = /[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+/;
     if (Regex.test(correo))
         console.log("El dato revisado SÍ es una dirección de correo electrónico.");
     else
         console.log("El dato revisado NO es una dirección de correo electrónico.");
 }
+
+export default checkEmail;
