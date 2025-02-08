6abb2645ac14e98330feb4555a01950275b06c9b
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-08 22:44:00 +0100

    Adding comments in main JS file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// Import statement.
 import { checkEmail } from './verify.js'
 
+// Declaring 2 variables to test function output.
 let direccion1 = "leo@academia-x.com";
 let direccion2 = "leo_academia-x.com";
 
+// Calling the function checkEmail which is declared in the module file. Testing 2 variables.
 checkEmail(direccion1);
 checkEmail(direccion2);
