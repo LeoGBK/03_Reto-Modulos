21c7087ba4cb0513fd8cae192f8174cc041eb69e
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-08 22:04:53 +0100

    Updating condition outcomes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function checkEmail(correo) {
     const Regex = /[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+/;
     if (Regex.test(correo)) {
-        return true;
+        console.log("El dato revisado SÍ es un correo electrónico.");
+        else {
+            console.log("El dato revisado NO es una dirección de correo electrónico.");
+        }
     }
 }
 
