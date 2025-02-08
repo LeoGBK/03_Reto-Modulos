60b6f20d72803ef8bc8dbf023c27f92817774341
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-08 22:02:37 +0100

    Creating variable and calling the function checkEmail()

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function checkEmail(correo){
+function checkEmail(correo) {
     const Regex = /[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+/;
     if (Regex.test(correo)) {
         return true;
     }
 }
 
+let direccion = "leo@academia-x.com";
+
+checkEmail(direccion);
