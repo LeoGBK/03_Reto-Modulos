20515605ab0407e26c6b96aca26bd1b709ddcd43
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-08 22:09:49 +0100

    Ehancing code

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function checkEmail(correo) {
     const Regex = /[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+/;
-    if (Regex.test(correo)) {
-        console.log("El dato revisado SÍ es un correo electrónico.");
-        else {
-            console.log("El dato revisado NO es una dirección de correo electrónico.");
-        }
-    }
+    if (Regex.test(correo))
+        console.log("El dato revisado SÍ es una dirección de correo electrónico.");
+    else
+        console.log("El dato revisado NO es una dirección de correo electrónico.");
 }
 
-let direccion = "leo@academia-x.com";
+let direccion1 = "leo@academia-x.com";
+let direccion2 = "leo_academia-x.com";
 
-checkEmail(direccion);
+checkEmail(direccion1);
+checkEmail(direccion2);
